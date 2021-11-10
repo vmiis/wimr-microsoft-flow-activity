e11e3f441bc207f9d5809cb7196ef56b911dbdd4
Zhiming Zhou <zzm.vmiis@gmail.com>
2021-11-10 12:18:46 +1100

    update

--- modules/module-list.js
+++ modules/module-list.js
@@ -5,8 +5,8 @@
     var m=$vm.module_list;
     var api="wimr";
     //-------------------------------------------------------------------------------------
-    m[p+"all"]     ={url:H+"/business/all/page.html",Table:'flow-activity',form_module:'jsonv',prefix:p,router:1};
-    m[p+"sleep-study-done-but-not-sent"]={url:H+"/business/sleep-study-done-but-not-sent/page.html",prefix:p,router:1};
-    m[p+"number-of-sleep-study-by-year-and-month"]={url:H+"/business/number-of-sleep-study-by-year-and-month/page.html",prefix:p,router:1};
-    m[p+"number-of-sleep-study-by-year"]={url:H+"/business/number-of-sleep-study-by-year/page.html",prefix:p,router:1};
+    m[p+"all"]                          ={url:H+"/business/all/page.html",Table:'flow-activity',form_module:'jsonv',prefix:p,router:1};
+    m[p+"snapforms-sleep-registration"] ={url:H+"/business/snapforms-sleep-registration/page.html",Table:'flow-activity',form_module:'jsonv',prefix:p,router:1};
+    m[p+"number-of-sleep-study-by-year-and-month"]={url:H+"/business/number-of-sleep-study-by-year-and-month/page.html",Table:'flow-activity',form_module:'jsonv',prefix:p,router:1};
+    m[p+"number-of-sleep-study-by-year"]={url:H+"/business/number-of-sleep-study-by-year/page.html",Table:'flow-activity',form_module:'jsonv',prefix:p,router:1};
 })();
