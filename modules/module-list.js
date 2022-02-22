94b42ddf7ebd2fb1b34197e221728588a8221d24
Zhiming Zhou <zzm.vmiis@gmail.com>
2022-02-22 13:05:28 +1100

    update

--- modules/module-list.js
+++ modules/module-list.js
@@ -5,11 +5,13 @@
     var m=$vm.module_list;
     var api="wimr";
     //-------------------------------------------------------------------------------------
-    m[p+"all"]                          ={url:H+"/business/all/page.html",Table:'flow-activity',form_module:'jsonv',prefix:p,router:1};
-    m[p+"today"]                        ={url:H+"/business/today/page.html",Table:'flow-activity',form_module:'jsonv',prefix:p,router:1};
-    m[p+"yesterday"]                    ={url:H+"/business/yesterday/page.html",Table:'flow-activity',form_module:'jsonv',prefix:p,router:1};
+    m[p+"all"]                                  ={url:H+"/business/all/page.html",Table:'flow-activity',form_module:'jsonv',prefix:p,router:1};
+    m[p+"today"]                                ={url:H+"/business/today/page.html",Table:'flow-activity',form_module:'jsonv',prefix:p,router:1};
+    m[p+"yesterday"]                            ={url:H+"/business/yesterday/page.html",Table:'flow-activity',form_module:'jsonv',prefix:p,router:1};
+    m[p+"rat"]                                  ={url:H+"/business/rat/page.html",Table:'flow-activity',form_module:'jsonv',prefix:p,router:1};
+    m[p+"sleep-study-pres-assessment-phone"]    ={url:H+"/business/sleep-study-pres-assessment-phone/page.html",Table:'flow-activity',form_module:'jsonv',prefix:p,router:1};
 
-    
+        
     m[p+"snapforms-sleep-registration"] ={url:H+"/business/snapforms-sleep-registration/page.html",Table:'flow-activity',form_module:'jsonv',prefix:p,router:1};
     m[p+"number-of-sleep-study-by-year-and-month"]={url:H+"/business/number-of-sleep-study-by-year-and-month/page.html",Table:'flow-activity',form_module:'jsonv',prefix:p,router:1};
     m[p+"number-of-sleep-study-by-year"]={url:H+"/business/number-of-sleep-study-by-year/page.html",Table:'flow-activity',form_module:'jsonv',prefix:p,router:1};
